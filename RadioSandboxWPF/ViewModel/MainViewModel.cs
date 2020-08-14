2eab531bcc4d1a8d50e9176f0066490b57efa6c1
Patrick Adesso <padesso@deximaging.com>
2020-08-14 16:34:04 -0400

    MVVM Light scaffold.

---
+++ RadioSandboxWPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadioSandboxWPF.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _testString;
+
+        public MainViewModel()
+        {
+            TestString = "this is a test";
+        }
+
+        public string TestString 
+        { 
+            get { return _testString; }
+            set 
+            { 
+                _testString = value;
+                RaisePropertyChanged("TestString");
+            }
+        }
+    }
+}
---
+++ RadioSandboxWPF/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,18 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadioSandboxWPF.ViewModel
+{
+    public class ViewModelLocator
+    {
+
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel MainViewModel => SimpleIoc.Default.GetInstance<MainViewModel>();
+    }
+}
