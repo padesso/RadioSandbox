c048780a40367d8f5f9b6c5289ac10c383530691
Patrick Adesso <padesso@deximaging.com>
2020-08-20 16:02:25 -0400

    WIP CW filter

--- RadioSandboxLibrary/Decoding/Morse/MorseDecoder.cs
+++ RadioSandboxLibrary/Decoding/Morse/MorseDecoder.cs
@@ -1,4 +1,5 @@
-﻿using NAudio.Utils;
+﻿using NAudio.Dsp;
+using NAudio.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,6 +10,9 @@ namespace RadioSandboxLibrary.Decoding.Morse
 {
     public class MorseDecoder : IDecoder
     {
+        private const float RISING_EDGE_THRESHOLD = 8000f; //TODO: use better values
+        private const float FALLING_EDGE_THRESHOLD = 2000f; //TODO: use better values
+
         private int sRate;
         private int bufferLength;
         private double secondsPerSample;
@@ -16,7 +20,8 @@ namespace RadioSandboxLibrary.Decoding.Morse
 
         private StringBuilder decodedString;
 
-        private double centerFrequency;
+        private float centerFrequency;
+        private BiQuadFilter cwFilter;
 
         private double timeUnitLength;
         private int dotLengthMultiple = 1;
@@ -30,6 +35,9 @@ namespace RadioSandboxLibrary.Decoding.Morse
             sRate = sampleRate;
             bufferLength = sRate;
             secondsPerSample = 1.0 / sampleRate;
+
+            centerFrequency = 15.0f; //TODO: use a real default value
+            cwFilter = BiQuadFilter.BandPassFilterConstantPeakGain(sampleRate, centerFrequency, 1.0f);
 
             decodedString = new StringBuilder();
             signalBuffer = new CircularBuffer(bufferLength);
@@ -77,10 +85,46 @@ namespace RadioSandboxLibrary.Decoding.Morse
             //TODO: iterate through the bytes and find the unit length
             double tempUnitLength = -1;
 
+            //TODO: walk through and find rising edges
+            float lastReading = 0;
+            for (int index = 0; index < currentBytes.Length; index += 2)
+            {
+                short sample = (short)((currentBytes[index + 1] << 8) |
+                                        currentBytes[index + 0]);
+                // to floating point
+                var sample32 = sample / 32768f;
+                // absolute value 
+                if (sample32 < 0) sample32 = -sample32;
+                // is this the max value?
+                //if (sample32 > max) max = sample32;
+
+                if(sample32 > lastReading) //Rising
+                {
+                    if(sample32 > RISING_EDGE_THRESHOLD)
+                    {
+                        //TODO
+                    }
+                }
+                else if (sample32 < lastReading) //Falling
+                {
+                    if (sample32 < FALLING_EDGE_THRESHOLD)
+                    {
+                        //TODO
+                    }
+                }
+                else
+                {
+                    //TODO: Not sure what we need to do here
+                }
+
+                lastReading = sample32;
+            }
+
             return tempUnitLength;
         }
 
         public string DecodedString { get => decodedString.ToString(); }
-        public double CenterFrequency { get => centerFrequency; set => centerFrequency = value; }
+
+        public float CenterFrequency { get => centerFrequency; set => centerFrequency = value; }
     }
 }
