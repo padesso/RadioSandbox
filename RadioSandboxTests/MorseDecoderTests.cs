2ca53126ced54890fd8b4913e112dbb8c7813c67
Patrick Adesso <padesso@deximaging.com>
2020-08-30 14:18:50 -0400

    Test WIP

--- RadioSandboxTests/MorseDecoderTests.cs
+++ RadioSandboxTests/MorseDecoderTests.cs
@@ -69,12 +69,11 @@ namespace RadioSandboxTests
         }
 
         [TestMethod]
-        public void DecodeFileFFTTest()
+        public void FileFFTTest()
         {
             //Load wav file contianing morse code, tone is 1000Hz
             (int sampleRate, double[] audio) = WavFile.ReadMono(@"Media\LS2-beacon.wav");
 
-            
             //Create the spectrogram to process the sample file
             Spectrogram.Spectrogram spec = new Spectrogram.Spectrogram(
                 sampleRate,
@@ -87,55 +86,7 @@ namespace RadioSandboxTests
 
             //Setup some filtering data
             float centerFrequency = 1500f; //Toner freq in Hz
-            int centerBandIndex = (int)((centerFrequency / (spec.FreqMax - spec.FreqMin)) * fftsProcessed[0].Length) - 1;
-            
-            bool isHigh = false;
-            List<int> risingEdgeIndices = new List<int>();
-            List<int> fallingEdgeIndices = new List<int>();
-
-            //TODO: this needs to average several bands...
-            //Iiterate through the processed data in the frequency bands we care about and decode morse //TODO: include the side bands
-            for (int processedIndex = 0; processedIndex < fftsProcessed.Length; processedIndex++) //Iterate all readings
-            {
-                //TODO: make this more flexible
-                double averagedReading = 0;
-                for (int bandpassOffset = -10; bandpassOffset <= 10; bandpassOffset++)
-                {
-                    averagedReading += fftsProcessed[processedIndex][centerBandIndex + bandpassOffset];
-                }
-                averagedReading /= 3.0;
-
-                if (averagedReading > 0.5) //TODO: better thresholds
-                {
-                    if (!isHigh)
-                    {
-                        risingEdgeIndices.Add(processedIndex);
-                        isHigh = true;
-                    }
-                }
-                else
-                {
-                    if (isHigh)
-                    {
-                        fallingEdgeIndices.Add(processedIndex);
-                        isHigh = false;
-                    }
-                }
-            }
-
-            //TODO: calculate some times
-            //string decodedText = "";
-            //List<double> highTimes = new List<double>();
-            //for (int highIndex = 0; highIndex < risingEdgeIndices.Count; highIndex++)
-            //{
-            //    highTimes.Add(spec.SecPerPx * (highIndex / fftsProcessed.Length));
-            //}
-
-            //The sample audio should rise and fall 12 times each
-            Assert.AreEqual(12, risingEdgeIndices.Count);
-            Assert.AreEqual(12, fallingEdgeIndices.Count);
-
-            //Assert.AreEqual(decodedText, "LS2");
+            int centerBandIndex = (int)((centerFrequency / (spec.FreqMax - spec.FreqMin)) * fftsProcessed[0].Length) - 1;  
         }
     }
 }
